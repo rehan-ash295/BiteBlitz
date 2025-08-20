9f9af1f54c3dd5b835ac52b98a91cb89336702f8
rehan-ash295 <rehanashfaque.303@gmail.com>
2025-08-20 20:20:03 +0530

    changed BE

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 const router = require("./Routes/CreateUser");
 const main = require("./db")
 const displaydata= require("./Routes/DisplayData");
@@ -11,7 +11,7 @@ const Orderdata= require("./Routes/OrderData");
 
 main();
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://bite-blitz-three.vercel.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
